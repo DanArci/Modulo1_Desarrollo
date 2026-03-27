c9882b1817609acedac7ad13215a305b8f09146a
DanArci <DANIELARCINIEGASPUA@GMAIL.COM>
2026-03-27 16:03:48 -0500

    Update files.py

--- semana_3/files.py
+++ semana_3/files.py
@@ -1,6 +1,6 @@
 import csv
 from services import userInput
-inventory = [{"name": 'LIMONADA', "price": '100', "quantity": '2'},{"name": 'TELEFONO', "price": '2000', "quantity": '4'}]
+inventory = [{"name": 'LIMONADA', "price": '100', "quantity": '2'},{"name": 'PANTALLA', "price": '2000', "quantity": '4'}]
 
 def save_csv(inventory, route, include_header=True):
     import os
@@ -18,8 +18,7 @@ def save_csv(inventory, route, include_header=True):
                 writer.writerow(['Name', 'Price', 'Quantity'])
             
             for product in inventory:
-                    writer.writerow([product['name'],product['price'],product['quantity']])
-                    
+                writer.writerow([product['name'],product['price'],product['quantity']])
                     
     except PermissionError:
         print('You do not have permissions for this file.')
@@ -63,25 +62,45 @@ def fuse_CSV(inventory,route):
             "quantity": int
         }
     products = []
+    success = []
+    fusing_data = []
         
     with open(route, 'r+', newline='') as f:
-        reader = csv.reader(f, delimiter = ',')
-        next(reader)
+        reader = csv.DictReader(f, delimiter = ',')
         db = list(reader)
-        
+        print(db)
+        print()
         for i in range(len(db)):
-            product['name'] = db[i][0]
-            product['price'] = db[i][1]   
-            product['quantity'] = db[i][2]
+            
+            product['name'] = db[i]['Name']
+            product['price'] =  db[i]['Price']
+            product['quantity'] = db[i]['Quantity']
+            
             for i in range(len(inventory)): 
                 if product['name'] == inventory[i]['name']:
                     product['price'] = inventory[i]['price']
                     product['quantity'] = int(product['quantity']) + int(inventory[i]['quantity'])
-            products.append(product.copy())
             
-    save_csv(products,'semana_3/db.csv',True)
+            success.append(product['name'])
+            
+            fusing_data.append([inventory[i]['name'],inventory[i]['price'],inventory[i]['quantity']])
+            
+            products.append(product.copy())
+        print(success)
+        print(fusing_data)
+        for i in range(len(success)-1):
+                if success[i] == fusing_data[i][0]:
+                    fusing_data[i][0] = 'null'
+                    print(fusing_data)
+                if fusing_data != 'null':
+                    products.append({"name": fusing_data[i][0], "price": fusing_data[i][1], "quantity": fusing_data[i][2]})
+        # print()
+        # print(db)
+        # print(products)        
+    overwrite_csv(products, 'Modulo1_Desarrollo/semana_3/db.csv',True)    
 
 def load_CSV(route):
+
     try:
         product = {
             "name": str,
@@ -116,7 +135,7 @@ def load_CSV(route):
                 if int(product['quantity']) < 0 or float(product['price']) < 1:
                     raise ValueError
                 
-                print(f'{product['name']} | {product['price']} | {product['quantity']}')
+                print(f"{product['name']} | {product['price']} | {product['quantity']}")
                 
             except ValueError:
                     error_rows += 1
@@ -145,3 +164,6 @@ def load_CSV(route):
         
     print(f'We omitted {error_rows} rows that contained an error')
     print(f'Action: {action}')
+
+#save_csv(inventory, 'Modulo1_Desarrollo/semana_3/db.csv', True)
+fuse_CSV(inventory, 'Modulo1_Desarrollo/semana_3/db.csv')
